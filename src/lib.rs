222acf7ba2b68c9fb7435436fe0a46f2d811e40e
diogogomesaraujo <118282799+diogogomesaraujo@users.noreply.github.com>
2025-03-03 17:18:15 +0000

    documentation

--- src/lib.rs
+++ src/lib.rs
@@ -1,18 +1,26 @@
+//! This file contains all the functions to generate and share secret keys. The secret keys' bit size can be ajusted by changing the const.
+
 use rug::{rand::RandState, Integer};
 
 mod modular;
 
+/// Const value of the Integers' size in bits.
 pub const BITS: u32 = 256;
 
+/// Function that calculates the biggest prime for the number of bits available.
 pub fn calculate_biggest_prime(rnd: &mut RandState) -> Integer {
     loop {
         let candidate = Integer::from(Integer::random_bits(BITS, rnd));
-        if candidate.is_probably_prime(30) != rug::integer::IsPrime::No {
-            return candidate;
+        match candidate.is_probably_prime(30) {
+            rug::integer::IsPrime::No => {
+                return candidate;
+            }
+            _ => continue,
         }
     }
 }
 
+//// Function that calculates the y value for a given polinomial and an x value.
 pub fn calculate_y(x: Integer, pol: &[Integer], prime: &Integer) -> Integer {
     pol.iter().enumerate().fold(Integer::ZERO, |acc, (i, p)| {
         modular::add(
@@ -27,6 +35,7 @@ pub fn calculate_y(x: Integer, pol: &[Integer], prime: &Integer) -> Integer {
     })
 }
 
+/// Function that calculates the lagrange polinomial (it is the algorythm used to recover a secret).
 fn lagrange_pol(x: Integer, pol: &[(Integer, Integer)], prime: &Integer) -> Integer {
     let n = pol.len();
     let mut result = Integer::from(0);
@@ -57,6 +66,7 @@ fn lagrange_pol(x: Integer, pol: &[(Integer, Integer)], prime: &Integer) -> Inte
     result
 }
 
+/// Function that generates a unique number for a given vector.
 fn generate_unique(rnd: &mut RandState, v: &[Integer]) -> Integer {
     let r = Integer::from(Integer::random_bits(BITS, rnd));
 
@@ -66,6 +76,7 @@ fn generate_unique(rnd: &mut RandState, v: &[Integer]) -> Integer {
     }
 }
 
+/// Function that generates a polinomial (it is used to divide the secret into multiple shares).
 fn generate_pol(key: Integer, k: u64, rnd: &mut RandState) -> Vec<Integer> {
     let mut pol: Vec<Integer> = vec![key];
 
@@ -77,6 +88,7 @@ fn generate_pol(key: Integer, k: u64, rnd: &mut RandState) -> Vec<Integer> {
     pol
 }
 
+/// Function that creates the secret shares.
 pub fn create_secret_shares(
     key: Integer,
     k: u64,
@@ -100,30 +112,14 @@ pub fn create_secret_shares(
     shares
 }
 
-pub fn recover_secret(shares: &[(Integer, Integer)], prime: &Integer) -> Integer {
-    lagrange_pol(Integer::from(0), shares, prime)
+/// Function that generates the secret key (if needed).
+pub fn generate_key(rnd: &mut RandState, prime: &Integer) -> Integer {
+    Integer::from(Integer::random_bits(BITS, rnd)).modulo(&prime)
 }
 
-#[test]
-fn test_create_recover() {
-    let mut rnd = RandState::new();
-
-    let prime = calculate_biggest_prime(&mut rnd);
-
-    let key = Integer::from(Integer::random_bits(BITS, &mut rnd)).modulo(&prime);
-    let k = 10;
-    let n = 15;
-
-    let shares = create_secret_shares(key.clone(), k, n, &prime);
-    let subset = &shares[0..(k as usize)];
-
-    let recovered_key = recover_secret(subset, &prime);
-
-    assert_eq!(
-        key, recovered_key,
-        "Secret Shares: {:?} \n{key} compared to {recovered_key}\n",
-        shares
-    );
+/// Function that recovers the secret.
+pub fn recover_secret(shares: &[(Integer, Integer)], prime: &Integer) -> Integer {
+    lagrange_pol(Integer::from(0), shares, prime)
 }
 
 #[test]
@@ -136,7 +132,7 @@ fn test_create_recover_bulk() {
             let prime = calculate_biggest_prime(&mut rnd);
 
             for _i in 0..100000 {
-                let key = Integer::from(Integer::random_bits(BITS, &mut rnd)).modulo(&prime);
+                let key = generate_key(&mut rnd, &prime);
                 let k = 2;
                 let n = 3;
 
--- src/main.rs
+++ src/main.rs
@@ -1,1 +1,19 @@
-fn main() {}
+use rug::{rand::RandState, Integer};
+use sss::*;
+
+fn main() {
+    let mut rnd = RandState::new();
+
+    let prime = calculate_biggest_prime(&mut rnd);
+
+    let key = generate_key(&mut rnd, &prime);
+    let k = 10;
+    let n = 15;
+
+    let shares = create_secret_shares(key.clone(), k, n, &prime);
+    let subset = &shares[0..(k as usize)];
+
+    let recovered_key = recover_secret(subset, &prime);
+
+    println!("Key: {key}\n\nRecovered Key: {recovered_key}");
+}
