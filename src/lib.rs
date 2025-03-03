2260602c734786d111697cc95384fc8479d8a52c
diogogomesaraujo <118282799+diogogomesaraujo@users.noreply.github.com>
2025-03-03 16:12:16 +0000

    working with rug integers

--- src/lib.rs
+++ src/lib.rs
@@ -1,106 +1,157 @@
-use rand::{rngs::ThreadRng, Rng};
+use rug::{rand::RandState, Integer};
 
 mod modular;
 
-pub const RANGE: std::ops::Range<u64> = 0..3657500100;
+pub const BITS: u32 = 256;
 
-pub const PRIME: u64 = 3657500101;
+pub fn calculate_biggest_prime(rnd: &mut RandState) -> Integer {
+    loop {
+        let candidate = Integer::from(Integer::random_bits(BITS, rnd));
+        if candidate.is_probably_prime(30) != rug::integer::IsPrime::No {
+            return candidate;
+        }
+    }
+}
 
-pub fn calculate_y(x: u64, pol: &[u64]) -> u64 {
-    pol.iter().enumerate().fold(0, |acc, (i, &p)| {
+pub fn calculate_y(x: Integer, pol: &[Integer], prime: Integer) -> Integer {
+    pol.iter().enumerate().fold(Integer::ZERO, |acc, (i, p)| {
         modular::add(
             acc,
-            modular::mul(p, modular::pow(x, i as u64, PRIME), PRIME),
-            PRIME,
+            modular::mul(
+                p.clone(),
+                modular::pow(x.clone(), Integer::from(i.clone()), prime.clone()),
+                prime.clone(),
+            ),
+            prime.clone(),
         )
     })
 }
 
-fn lagrange_pol(x: u64, pol: &[(u64, u64)]) -> u64 {
+fn lagrange_pol(x: Integer, pol: &[(Integer, Integer)], prime: Integer) -> Integer {
     let n = pol.len();
-    let mut result = 0;
+    let mut result = Integer::from(0);
 
     for i in 0..n {
-        let (xi, yi) = pol[i];
+        let (xi, yi) = pol[i].clone();
 
-        let mut num = 1;
-        let mut den = 1;
+        let mut num = Integer::from(1);
+        let mut den = Integer::from(1);
 
         for j in 0..n {
             if j != i {
-                let (xj, _) = pol[j];
-                num = modular::mul(num, modular::sub(x, xj, PRIME), PRIME);
-                den = modular::mul(den, modular::sub(xi, xj, PRIME), PRIME);
+                let (xj, _) = pol[j].clone();
+                num = modular::mul(
+                    num.clone(),
+                    modular::sub(
+                        Integer::from(x.clone()),
+                        Integer::from(xj.clone()),
+                        prime.clone(),
+                    ),
+                    prime.clone(),
+                );
+                den = modular::mul(
+                    den,
+                    modular::sub(xi.clone(), xj, prime.clone()),
+                    prime.clone(),
+                );
             }
         }
-        let div = modular::div(num, den, PRIME);
-        let term = modular::mul(yi, div, PRIME);
+        let div = modular::div(num, den, prime.clone());
+        let term = modular::mul(yi, div, prime.clone());
 
-        result = modular::add(result, term, PRIME);
+        result = modular::add(result, term, prime.clone());
     }
 
     result
 }
 
-fn generate_unique(rgn: &mut ThreadRng, v: &[u64]) -> u64 {
-    let r: u64 = rgn.random_range(RANGE);
+fn generate_unique(rnd: &mut RandState, v: &[Integer]) -> Integer {
+    let r = Integer::from(Integer::random_bits(BITS, rnd));
 
-    match v.contains(&r) || r == 0 {
-        true => generate_unique(rgn, v),
-        false => r,
+    match v.iter().find(|&i| i == &r) {
+        Some(_) => generate_unique(rnd, v),
+        None => r,
     }
 }
 
-fn generate_pol(key: u64, k: u64, rgn: &mut ThreadRng) -> Vec<u64> {
-    let mut pol: Vec<u64> = vec![key];
+fn generate_pol(key: Integer, k: u64, rnd: &mut RandState) -> Vec<Integer> {
+    let mut pol: Vec<Integer> = vec![key];
 
     for _i in 1..k {
-        let r: u64 = generate_unique(rgn, &pol);
+        let r = generate_unique(rnd, &pol);
         pol.push(r);
     }
 
     pol
 }
 
-pub fn create_secret_shares(key: u64, k: u64, n: u64) -> Vec<(u64, u64)> {
-    let mut rgn = rand::rng();
+pub fn create_secret_shares(
+    key: Integer,
+    k: u64,
+    n: u64,
+    prime: Integer,
+) -> Vec<(Integer, Integer)> {
+    let mut rnd = RandState::new();
 
-    let pol = generate_pol(key, k, &mut rgn);
-    let mut shares: Vec<(u64, u64)> = Vec::new();
+    let pol = generate_pol(key, k, &mut rnd);
+    let mut shares: Vec<(Integer, Integer)> = Vec::new();
     let mut xs = Vec::new();
 
     for _i in 0..n {
-        let x = generate_unique(&mut rgn, &xs);
-        xs.push(x);
+        let x = generate_unique(&mut rnd, &xs);
+        xs.push(x.clone());
 
-        let y = calculate_y(x, &pol);
-        shares.push((x, y));
+        let y = calculate_y(x.clone(), &pol, prime.clone());
+        shares.push((x.clone(), y));
     }
 
     shares
 }
 
-pub fn recover_secret(shares: &[(u64, u64)]) -> u64 {
-    lagrange_pol(0, shares)
+pub fn recover_secret(shares: &[(Integer, Integer)], prime: Integer) -> Integer {
+    lagrange_pol(Integer::from(0), shares, prime)
 }
 
 #[test]
 fn test_create_recover() {
+    let mut rnd = RandState::new();
+
+    let prime = calculate_biggest_prime(&mut rnd);
+
+    let key = Integer::from(Integer::random_bits(BITS, &mut rnd)).modulo(&prime);
+    let k = 10;
+    let n = 15;
+
+    let shares = create_secret_shares(key.clone(), k, n, prime.clone());
+    let subset = &shares[0..(k as usize)];
+
+    let recovered_key = recover_secret(subset, prime);
+
+    assert_eq!(
+        key, recovered_key,
+        "Secret Shares: {:?} \n{key} compared to {recovered_key}\n",
+        shares
+    );
+}
+/*
+#[test]
+fn test_create_recover_bulk() {
     let mut handles = Vec::new();
 
-    for _i in 0..5 {
+    for _i in 0..100 {
         let handle = std::thread::spawn(|| {
-            let mut rgn = rand::rng();
+            let mut rnd = RandState::new();
+            let prime = calculate_biggest_prime(&mut rnd);
 
-            for _i in 0..250000 {
-                let key: u64 = rgn.random_range(RANGE);
-                let k = 5;
-                let n = 5;
+            for _i in 0..10000 {
+                let key = Integer::from(Integer::random_bits(BITS, &mut rnd)).modulo(&prime);
+                let k = 2;
+                let n = 3;
 
-                let shares = create_secret_shares(key, k, n);
+                let shares = create_secret_shares(key.clone(), k, n, prime.clone());
                 let subset = &shares[0..(k as usize)];
 
-                let recovered_key = recover_secret(subset);
+                let recovered_key = recover_secret(subset, prime.clone());
 
                 assert_eq!(
                     key, recovered_key,
@@ -116,3 +167,4 @@ fn test_create_recover() {
         handle.join().unwrap();
     }
 }
+*/
--- src/modular.rs
+++ src/modular.rs
@@ -1,41 +1,26 @@
-pub fn add(a: u64, b: u64, m: u64) -> u64 {
-    ((a.rem_euclid(m)).wrapping_add(b.rem_euclid(m))).rem_euclid(m)
+use rug::Integer;
+
+pub fn add(a: Integer, b: Integer, m: Integer) -> Integer {
+    ((a.modulo(&m)) + (b.modulo(&m))).modulo(&m)
 }
 
-pub fn sub(a: u64, b: u64, m: u64) -> u64 {
-    ((a.rem_euclid(m))
-        .wrapping_sub(b.rem_euclid(m))
-        .wrapping_add(m))
-    .rem_euclid(m)
+pub fn sub(a: Integer, b: Integer, m: Integer) -> Integer {
+    ((a.modulo(&m)) - (b.modulo(&m)) + &m).modulo(&m)
 }
 
-pub fn mul(a: u64, b: u64, m: u64) -> u64 {
-    ((a.rem_euclid(m)).wrapping_mul(b.rem_euclid(m))).rem_euclid(m)
+pub fn mul(a: Integer, b: Integer, m: Integer) -> Integer {
+    ((a.modulo(&m)) * (b.modulo(&m))).modulo(&m)
 }
 
-pub fn inverse(a: u64, m: u64) -> u64 {
-    pow(a, (m.wrapping_sub(2)) as u64, m)
+pub fn div(a: Integer, b: Integer, m: Integer) -> Integer {
+    let a = a.modulo(&m);
+    let inv = b.invert(&m).expect("No modular inverse exists");
+    (inv * a).modulo(&m)
 }
 
-pub fn div(a: u64, b: u64, m: u64) -> u64 {
-    let a = a.rem_euclid(m);
-    let inv = inverse(b, m);
-    (inv.wrapping_mul(a)).rem_euclid(m)
-}
-
-pub fn pow(x: u64, y: u64, p: u64) -> u64 {
-    let mut res: u64 = 1;
-    let mut x = x.rem_euclid(p);
-    let mut y = y;
-    if x == 0 {
-        return 0;
+pub fn pow(x: Integer, y: Integer, p: Integer) -> Integer {
+    match x.pow_mod(&y, &p) {
+        Ok(i) => i,
+        Err(_) => unreachable!(),
     }
-    while y > 0 {
-        if (y & 1) != 0 {
-            res = (res.wrapping_mul(x)).rem_euclid(p);
-        }
-        y >>= 1;
-        x = (x.wrapping_mul(x)).rem_euclid(p);
-    }
-    res
 }
