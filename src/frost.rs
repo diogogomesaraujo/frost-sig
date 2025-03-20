6fa7c6b38948d1d2fa67b68103b3e36659f058ab
diogogomesaraujo <118282799+diogogomesaraujo@users.noreply.github.com>
2025-03-20 17:52:52 +0000

    key gen round one working and complete

--- src/frost.rs
+++ src/frost.rs
@@ -1,10 +1,14 @@
-use crate::{modular, BITS, PRIME};
+use crate::{modular, PRIME};
+use commitments_and_proofs::{
+    compute_proof_of_knowlodge, compute_public_commitments, verify_proofs,
+};
+use rand::Rng;
 use rug::{rand::RandState, Integer};
 use sha256::digest;
 use std::str::FromStr;
 
 pub struct FrostState {
-    participant_id: Integer,
+    pub participant_id: Integer,
     pub prime: Integer,
     pub q: Integer,
     pub generator: Integer,
@@ -32,6 +36,7 @@ impl FrostState {
     }
 }
 
+#[derive(Clone)]
 pub struct CTX {
     pub protocol: String,
     pub group_id: Integer,
@@ -48,15 +53,11 @@ impl CTX {
     }
 
     pub fn to_string(ctx: &CTX) -> String {
-        format!(
-            "{}::{}::{}",
-            ctx.protocol,
-            ctx.group_id.to_string_radix(16),
-            ctx.session_id.to_string_radix(16)
-        )
+        format!("{}::{}::{}", ctx.protocol, ctx.group_id, ctx.session_id)
     }
 }
 
+#[derive(Clone)]
 pub struct ParticipantBroadcast {
     pub participant_id: Integer,
     pub commitments: Vec<Integer>,
@@ -77,78 +78,144 @@ impl ParticipantBroadcast {
     }
 }
 
-pub fn generate_polynomial(state: &FrostState, rnd: &mut RandState) -> Vec<Integer> {
-    let mut polynomial: Vec<Integer> = Vec::new();
-    for _i in 0..state.threshold {
-        let a = Integer::from(Integer::random_below(state.q.clone(), rnd));
-        polynomial.push(a);
+pub fn generate_integer(state: &FrostState, rnd: &mut RandState) -> Integer {
+    Integer::from(Integer::random_below(state.q.clone(), rnd))
+}
+
+pub mod commitments_and_proofs {
+    use super::*;
+
+    pub fn generate_polynomial(state: &FrostState, rnd: &mut RandState) -> Vec<Integer> {
+        let mut polynomial: Vec<Integer> = Vec::new();
+        for _i in 0..state.threshold {
+            let a = generate_integer(&state, rnd);
+            polynomial.push(a);
+        }
+        polynomial
     }
-    polynomial
-}
 
-pub fn compute_proof_of_knowlodge(
-    state: &FrostState,
-    rnd: &mut RandState,
-    generated_polynomial: &[Integer],
-) -> (Integer, Integer) {
-    let k = Integer::from(Integer::random_below(state.q.clone(), rnd));
-    let r = modular::pow(&state.generator, &k, &state.prime);
-    let ci = Integer::from_str_radix(
-        digest(format!(
-            "{}::::{}:::::{}:::::{}",
-            &state.participant_id.to_string_radix(16),
-            CTX::to_string(&state.ctx),
-            modular::pow(&state.generator, &generated_polynomial[0], &state.prime),
-            r
-        ))
-        .as_str(),
-        16,
-    )
-    .unwrap()
-    .modulo(&state.q);
-    let wi = modular::add(
-        k,
-        modular::mul(generated_polynomial[0].clone(), ci.clone(), &state.q),
-        &state.q,
-    );
-    (wi, ci)
-}
-
-pub fn compute_public_commitments(
-    state: FrostState,
-    generated_polynomial: &[Integer],
-) -> Vec<Integer> {
-    generated_polynomial
-        .iter()
-        .map(|coefficient| modular::pow(&state.generator, &coefficient, &state.prime))
-        .collect()
-}
-
-pub fn verify_proofs(state: &FrostState, participants_broadcasts: &[ParticipantBroadcast]) -> bool {
-    participants_broadcasts.iter().fold(true, |acc, pb| {
-        let (wp, cp) = pb.signature.clone();
-
-        let ap0 = modular::pow(&pb.commitments[0], &Integer::from(-&cp), &state.prime);
-
-        let rp = modular::mul(
-            modular::pow(&state.generator, &wp, &state.prime),
-            ap0.clone(),
-            &state.prime,
+    pub fn compute_proof_of_knowlodge(
+        state: &FrostState,
+        rnd: &mut RandState,
+        generated_polynomial: &[Integer],
+    ) -> (Integer, Integer) {
+        let k = generate_integer(&state, rnd);
+        let r = modular::pow(&state.generator, &k, &state.prime);
+        let ci = Integer::from_str_radix(
+            digest(format!(
+                "{}::::{}::::{}::::{}",
+                &state.participant_id,
+                CTX::to_string(&state.ctx),
+                modular::pow(&state.generator, &generated_polynomial[0], &state.prime),
+                r
+            ))
+            .as_str(),
+            16,
+        )
+        .unwrap()
+        .modulo(&state.q);
+        let wi = modular::add(
+            k,
+            modular::mul(generated_polynomial[0].clone(), ci.clone(), &state.q),
+            &state.q,
         );
+        (wi, ci)
+    }
 
-        acc && (cp
-            == Integer::from_str_radix(
-                digest(format!(
-                    "{}::::{}::::{}::::{}",
-                    &pb.participant_id.to_string_radix(16),
-                    CTX::to_string(&state.ctx),
-                    ap0,
-                    rp
-                ))
-                .as_str(),
-                16,
-            )
-            .unwrap()
-            .modulo(&state.q))
-    })
+    pub fn compute_public_commitments(
+        state: &FrostState,
+        generated_polynomial: &[Integer],
+    ) -> Vec<Integer> {
+        generated_polynomial
+            .iter()
+            .map(|coefficient| modular::pow(&state.generator, &coefficient, &state.prime))
+            .collect()
+    }
+
+    pub fn verify_proofs(
+        state: &FrostState,
+        participants_broadcasts: &[ParticipantBroadcast],
+    ) -> bool {
+        participants_broadcasts.iter().fold(true, |acc, pb| {
+            let (wp, cp) = pb.signature.clone();
+            let rp = modular::mul(
+                modular::pow(&state.generator, &wp, &state.prime),
+                modular::pow(&pb.commitments[0], &Integer::from(-&cp), &state.prime),
+                &state.prime,
+            );
+            acc && (cp
+                == Integer::from_str_radix(
+                    digest(format!(
+                        "{}::::{}::::{}::::{}",
+                        &pb.participant_id,
+                        CTX::to_string(&state.ctx),
+                        &pb.commitments[0],
+                        rp,
+                    ))
+                    .as_str(),
+                    16,
+                )
+                .unwrap()
+                .modulo(&state.q))
+        })
+    }
+}
+
+#[test]
+pub fn test_keygen_commitments_and_proofs() {
+    let seed: i32 = rand::rng().random();
+    let mut rnd = RandState::new();
+    rnd.seed(&rug::Integer::from(seed));
+
+    let ctx = CTX::init(Integer::from(1), Integer::from(1));
+
+    let state_1 = FrostState::init(Integer::from(1), 3, 2, ctx.clone());
+    let state_2 = FrostState::init(Integer::from(2), 3, 2, ctx.clone());
+    let state_3 = FrostState::init(Integer::from(3), 3, 2, ctx.clone());
+
+    let pol_1 = commitments_and_proofs::generate_polynomial(&state_1, &mut rnd);
+    let pol_2 = commitments_and_proofs::generate_polynomial(&state_2, &mut rnd);
+    let pol_3 = commitments_and_proofs::generate_polynomial(&state_3, &mut rnd);
+
+    let signature_1 = compute_proof_of_knowlodge(&state_1, &mut rnd, &pol_1);
+    let signature_2 = compute_proof_of_knowlodge(&state_2, &mut rnd, &pol_2);
+    let signature_3 = compute_proof_of_knowlodge(&state_3, &mut rnd, &pol_3);
+
+    let commitments_1 = compute_public_commitments(&state_1, &pol_1);
+    let commitments_2 = compute_public_commitments(&state_2, &pol_2);
+    let commitments_3 = compute_public_commitments(&state_3, &pol_3);
+
+    let participant_broadcast_1 =
+        ParticipantBroadcast::init(state_1.participant_id.clone(), commitments_1, signature_1);
+    let participant_broadcast_2 =
+        ParticipantBroadcast::init(state_2.participant_id.clone(), commitments_2, signature_2);
+    let participant_broadcast_3 =
+        ParticipantBroadcast::init(state_3.participant_id.clone(), commitments_3, signature_3);
+
+    // verify the first one
+    assert!(verify_proofs(
+        &state_1,
+        &[
+            participant_broadcast_2.clone(),
+            participant_broadcast_3.clone()
+        ]
+    ));
+
+    // verify the second one
+    assert!(verify_proofs(
+        &state_2,
+        &[
+            participant_broadcast_1.clone(),
+            participant_broadcast_3.clone()
+        ]
+    ));
+
+    // verify the third one
+    assert!(verify_proofs(
+        &state_2,
+        &[
+            participant_broadcast_1.clone(),
+            participant_broadcast_3.clone()
+        ]
+    ));
 }
